e9333235661c6b5b80697cb0538024aa6ab404e1
jjj333_p <jjj333.p.1325@gmail.com>
2024-11-30 18:58:35 -1000

    panic: Could not login - unmarshal <features>: expected element type <features> but have <error>

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/xmppo/go-xmpp"
+	"log"
+	"os"
 	"strings"
 )
 
@@ -13,13 +15,37 @@ func serverName(host string) string {
 
 func main() {
 
+	loginJSONbytes, err := os.ReadFile("db/login.json")
+	if err != nil {
+		panic("Unable to read login.json - " + err.Error())
+	}
+
 	xmppConfig := xmpp.Options{}
 
-	if err := json.Unmarshal([]byte("{\"user\":\"meow\"}"), &xmppConfig); err != nil {
-		panic(err)
+	if err := json.Unmarshal(loginJSONbytes, &xmppConfig); err != nil {
+		panic("Could not parse login.json - " + err.Error())
 	}
 
 	fmt.Println(xmppConfig.User)
+
+	client, err := xmppConfig.NewClient()
+	if err != nil {
+		panic("Could not login - " + err.Error())
+	}
+
+	go func() {
+		//while chat, err := client.Recv(); chat != nil {
+		//
+		//}
+		for {
+			event, err := client.Recv()
+			if err != nil {
+				log.Println("Error receiving event - " + err.Error())
+			}
+
+			log.Println(event)
+		}
+	}()
 
 	//options := xmpp.Options{}
 }
