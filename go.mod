1e864a973a89e2b4e00c40af1d290e82b17a87a0
jjj333_p <jjj333.p.1325@gmail.com>
2024-11-30 18:27:55 -1000

    existing more

---
+++ go.mod
@@ -0,0 +1,9 @@
+module pain.agency/oasis
+
+go 1.22.9
+
+require (
+	github.com/xmppo/go-xmpp v0.2.7 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/xmppo/go-xmpp"
+	"strings"
+)
+
+func serverName(host string) string {
+	return strings.Split(host, ":")[0]
+}
+
 func main() {
 
+	xmppConfig := xmpp.Options{}
+
+	if err := json.Unmarshal([]byte("{\"user\":\"meow\"}"), &xmppConfig); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(xmppConfig.User)
+
+	//options := xmpp.Options{}
 }
